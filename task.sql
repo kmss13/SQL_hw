c1afd760d17c440964c274aa2d1b05f079444b5c
kmss13 <94681910+kmss13@users.noreply.github.com>
2024-03-26 23:36:33 +0400

    Update task.sql

--- task.sql
+++ task.sql
@@ -1,24 +1,13 @@
-
-
-
-
-
 CREATE PROCEDURE GetEvenNumbers()
-
 BEGIN
-
-    DECLARE x INT DEFAULT 2;
-	DROP TABLE IF EXISTS nums;
-    CREATE TABLE nums (x INT);
-
-    WHILE x < 10 DO
-
-    SET x = x + 2;
-	INSERT INTO nums VALUES(x);
-    END WHILE;
-
+    DECLARE x INT DEFAULT 0;
+    DROP TABLE IF EXISTS nums;
+    CREATE TEMPORARY TABLE nums (x INT);
+	WHILE x < 10 DO
+		SET x = x + 2;
+		INSERT INTO nums VALUES(x);
+	END WHILE;
     SELECT * FROM nums;
+END;
 
-END
-
-	CALL GetEvenNumbers();
+call GetEvenNumbers();
