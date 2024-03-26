3b5085e592d0e280e1da2533afdcc77f4ffd385c
kmss13 <94681910+kmss13@users.noreply.github.com>
2024-03-26 23:23:18 +0400

    Update task2.sql

--- task2.sql
+++ task2.sql
@@ -1,40 +1,20 @@
 CREATE FUNCTION getTime(seconds INT)
 RETURNS VARCHAR(100)
+DETERMINISTIC
 BEGIN
+ DECLARE days INT;
+ DECLARE hours INT;
+ DECLARE minutes INT;
+ DECLARE result VARCHAR(100);
  
-    DECLARE days INT;
- 
-    DECLARE hours INT;
- 
-    DECLARE minutes INT;
- 
-    DECLARE result VARCHAR(100);
- 
-    
- 
-    SET days = FLOOR(seconds / (24 * 60 * 60));
- 
-    SET seconds = seconds % (24 * 60 * 60);
- 
-    
- 
-    SET hours = FLOOR(seconds / (60 * 60));
- 
-    SET seconds = seconds % (60 * 60);
- 
-    
- 
-    SET minutes = FLOOR(seconds / 60);
- 
-    SET seconds = seconds % 60;
- 
-    
- 
-    SET result = CONCAT(days, ' days ', hours, ' hours ', minutes, ' minutes ', seconds, ' seconds');
- 
-    
- 
-    RETURN result;
- 
-END
- SELECT getTime(12345)
+ SET days = FLOOR(seconds / (24 * 60 * 60));
+ SET seconds = seconds % (24 * 60 * 60);
+ SET hours = FLOOR(seconds / (60 * 60));
+ SET seconds = seconds % (60 * 60);
+ SET minutes = FLOOR(seconds / 60);
+ SET seconds = seconds % 60;
+ SET result = CONCAT(days, ' days ', hours, ' hours ', minutes, ' minutes ', seconds, ' seconds');
+RETURN result;
+END;
+
+SELECT getTime(12345);
